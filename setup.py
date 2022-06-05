6703160b54f3e5925753a99ca4cf24bf48dc1c5c
AndreyAgeev <andreyageev1@mail.ru>
2022-06-05 19:17:13 +0300

    [BOSK-1, BOSK-2, BOSK-3] feat: Add setup.py, linter, docs by sphinx

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+import bosk
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# requirements = []
+
+setup(
+    install_requires=[],
+    name="bosk",
+    version=bosk.__version__,
+    author="NTAILab",
+    description="Deep Forest package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    # install_requires=requirements,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"),
+    license='MIT',
+    python_requires='>=3.6',
+)
