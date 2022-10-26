27acf45ff7d3fd67d008c4dd18f9ee9f26dc4e9b
AndreyAgeev <andreyageev1@mail.ru>
2022-10-26 12:51:31 +0300

    [BOSK-17] fix: Removed model indexes, added a cycle through models during training and prediction

--- bosk/block/zoo/multi_grained_scanning/multi_grained_scanning.py
+++ bosk/block/zoo/multi_grained_scanning/multi_grained_scanning.py
@@ -1,23 +1,19 @@
 from abc import ABC, abstractmethod
-from enum import IntEnum
-from typing import Any, List, Tuple, Optional
+from typing import Any, Tuple, Optional
 
 import numpy as np
 
 
-class ModelTypeIndex(IntEnum):
-    RANDOM = 0,
-    COMPLETELY_RANDOM = 1
-
-
 class MultiGrainedScanningBlock(ABC):
-    def __init__(self, models: Tuple[Any], window_size: int, stride: int, shape_sample: Optional[Tuple[int]] = None):
+    def __init__(self, models: Tuple[Any, Any],
+                 window_size: int,
+                 stride: int,
+                 shape_sample: Optional[Tuple[int]] = None):
         super().__init__()
         self._shape_sample = shape_sample
         self._window_size: int = window_size
         self._stride: int = stride
-        self._models: Tuple[Any, Any] = (models[ModelTypeIndex.RANDOM],
-                                         models[ModelTypeIndex.COMPLETELY_RANDOM])
+        self._models: Tuple[Any, Any] = models
 
     def fit(self, X, y) -> 'MultiGrainedScanningBlock':
         self._window_slicing_fit(X, y)
@@ -25,13 +21,12 @@ class MultiGrainedScanningBlock(ABC):
 
     def _window_slicing_fit(self, X, y) -> 'None':
         sliced_X, sliced_y = self._window_slicing_data(X, y)
-        prf = self._models[ModelTypeIndex.RANDOM]
-        crf = self._models[ModelTypeIndex.COMPLETELY_RANDOM]
-        if hasattr(prf, 'fit') and hasattr(prf, 'fit'):
-            prf.fit(sliced_X, sliced_y)
-            crf.fit(sliced_X, sliced_y)
-        else:
-            raise Exception("Please check that the models used in 'MultiGrainedScanningBlock' have the method 'fit'")
+        for model in self._models:
+            if hasattr(model, 'fit'):
+                model.fit(sliced_X, sliced_y)
+            else:
+                raise Exception("Please check that the model used in 'MultiGrainedScanningBlock' "
+                                "have the method 'fit'")
 
     @abstractmethod
     def _window_slicing_data(self, X, y=None) -> 'Tuple[np.ndarray, Optional[np.ndarray]]':
@@ -39,11 +34,16 @@ class MultiGrainedScanningBlock(ABC):
 
     def _window_slicing_predict(self, X) -> 'np.ndarray':
         sliced_X, _ = self._window_slicing_data(X)
-        pred_prob_prf = self._models[ModelTypeIndex.RANDOM].predict_proba(sliced_X)
-        pred_prob_crf = self._models[ModelTypeIndex.COMPLETELY_RANDOM].predict_proba(sliced_X)
-        pred_prob = np.c_[pred_prob_prf, pred_prob_crf]
-        return pred_prob.reshape([X.shape[0], -1])
+        predict_prob = np.array([])
+        for model in self._models:
+            if hasattr(model, 'predict_proba'):
+                predict_prob = np.hstack([predict_prob, model.predict_proba(sliced_X)]) \
+                    if predict_prob.size else model.predict_proba(sliced_X)
+            else:
+                raise Exception(
+                    "Please check that the model used in 'MultiGrainedScanningBlock'"
+                    " have the method 'predict_proba'")
+        return predict_prob.reshape([X.shape[0], -1])
 
     def transform(self, X) -> 'np.ndarray':
-        res = self._window_slicing_predict(X)
-        return res
+        return self._window_slicing_predict(X)
