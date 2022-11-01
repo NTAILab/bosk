969633c9ff9c91f6bc3f4c3cb503af532e6ef8e6
stasychbr <stasychbr@mail.ru>
2022-11-01 22:57:15 +0300

    Docstrings

--- bosk/executor/painter.py
+++ bosk/executor/painter.py
@@ -1,6 +1,18 @@
+"""Module, containing the mixin class with method 
+for drawing the computational graph, which was proceeded by some executor.
+"""
+
 from abc import ABC, abstractmethod
 
 class PainterMixin(ABC):
+    """Mixin class containing method for drawing the computational graph,
+    which was proceeded by some executor.
+    """
     @abstractmethod
     def draw(self, output_filename: str) -> None:
+        """Method for the computational graph drawing.
+        
+        Args:
+            output_filename: Path (containing the filename) where the output graphics file will be saved.
+        """
         ...
--- bosk/executor/topological.py
+++ bosk/executor/topological.py
@@ -1,3 +1,9 @@
+"""Topological executor with the graph painter.
+
+This file contains the optimizing executor, which also can draw the computational graph.
+
+"""
+
 from typing import Deque, Dict, List, Mapping, Sequence, Set
 from collections.abc import Iterable
 from collections import defaultdict, deque
@@ -14,7 +20,34 @@ import graphviz as gv
 
 
 class TopologicalExecutor(BaseExecutor, PainterMixin):
+    """Topological executor with the graph painter.
+    The optimization algoritm computes only blocks which are connected with the inputs and needed for 
+    the outputs calculation. You can see which blocks would be executed if you draw the computational graph
+    with the 'draw' method.
+    
+    Attributes:
+        conn_dict: Pipeline connections, represented as a hash map,
+        the keys are blocks' input slots, the values are output ones. Each input slot corresponds no more than one 
+        output slot, so this representation is correct.
+
+        slot_to_block_map: Dictionary that allows to determine to which block
+        some slot does belong. Will be removed in the future versions, when the BaseSlot will contain the link to his parent block.
+
+        levels_sep: The painter's parameter, which determines the distance between the computational graph's levels. See http://graphviz.org/docs/attrs/ranksep/.
+
+        dpi: The dpi of the output computational graph images, formated in raster graphics (.png, .jpeg, etc.).
+
+        rankdir: The direction of the computational graph edges. See https://graphviz.org/docs/attrs/rankdir/.
+    """
     def __check_inputs_concordance(self, input_values: Mapping[str, Data]) -> None:
+        """The function that checks if the input values, provided to the '__call__' method, agree with the pipeline's inputs.
+
+        Args:
+            input_values: Values, which were provided in the '__call__' method.
+        
+        Raises:
+            AssertionError: If there are some incompatibility between pipeline's inputs and user's ones.
+        """
         passed = 0
         for inp_name, inp_slot in self.inputs.items():
             assert (inp_name in input_values), f"Unable to find input slot {inp_name} (id {hash(inp_slot)}) in input data"
@@ -22,6 +55,8 @@ class TopologicalExecutor(BaseExecutor, PainterMixin):
         assert (passed == len(input_values)), "Input values are incompatible with pipeline input slots"
 
     def _get_slot_to_block_map(self) -> Mapping[BaseSlot, BaseBlock]:
+        """Method that creates 'slot_to_block_map'.
+        """
         slot_to_block_map: Mapping[BaseSlot, BaseBlock] = dict()
         for block in self.pipeline.nodes:
             for slot in block.slots.inputs.values():
@@ -32,10 +67,12 @@ class TopologicalExecutor(BaseExecutor, PainterMixin):
 
     # contains extra links for pipeline input slots (cyclic)
     def _get_connection_map(self) -> Mapping[BlockInputSlot, BlockOutputSlot]:
+        """Method that creates 'conn_dict' and checks if every input slot 
+        has no more than one corresponding output slot.
+        """
         conn_dict: Mapping[BlockInputSlot, BlockOutputSlot] = dict()
         for conn in self.pipeline.connections:
-            assert (
-                conn.dst not in conn_dict), f"Input slot {conn.dst.name} (id {hash(conn.dst)}) is used more than once"
+            assert (conn.dst not in conn_dict), f"Input slot {conn.dst.name} (id {hash(conn.dst)}) is used more than once"
             if self._is_input_slot_required(conn.dst):
                 conn_dict[conn.dst] = conn.src
         for input_or_inputs in self.inputs.values():
@@ -62,6 +99,18 @@ class TopologicalExecutor(BaseExecutor, PainterMixin):
         self.rankdir = figure_rankdir
 
     def _dfs(self, aj_list: Mapping[BaseBlock, BaseBlock], begin_nodes: Sequence[BaseBlock]) -> Set[BaseBlock]:
+        """Method that performs the deep first search algorithm in the computational graph.
+        The search begins from the nodes 'begin_nodes'. The algorithm is written using iterative scheme.
+
+        Args:
+            aj_list: The graph adjacency list.
+
+            begin_nodes: The graph nodes, which will be used as the search start.
+        
+        Returns:
+            Set of the graph nodes, which were included in the traversal. Actually, the order is not saved.
+            This method is only for internal use, so this behaviour is chosen for the optimization.
+        """
         visited: Set[BaseBlock] = set(begin_nodes)
         stack: Deque[BaseBlock] = deque(begin_nodes)
         while stack:
@@ -73,6 +122,21 @@ class TopologicalExecutor(BaseExecutor, PainterMixin):
         return visited
 
     def _topological_sort(self, aj_list: Mapping[BaseBlock, BaseBlock], begin_nodes: Sequence[BaseBlock]) -> List[BaseBlock]:
+        """Method that performs the topological sort of the computational graph.
+        The algorithm begins its work from the nodes 'begin_nodes'. The algorithm is written using recursive scheme.
+
+        Args:
+            aj_list: The graph adjacency list.
+
+            begin_nodes: The graph nodes, which will be used as the algorithm start.
+        
+        Returns:
+            List of the graph nodes in topological order. 
+
+        Todo:
+            Rewrite the sort with the iterative cheme. Also, think of the performing graph computation during the sort.
+
+        """
         visited: Set[BaseBlock] = set()
         stack: Deque[BaseBlock] = deque()
 
@@ -91,7 +155,17 @@ class TopologicalExecutor(BaseExecutor, PainterMixin):
         result.reverse()
         return result
 
-    def _get_backward_aj_list(self, feasible_set: Set[BaseBlock] = None) -> Mapping[BaseBlock, Set[BaseBlock]]:
+    def _get_backward_aj_list(self, feasible_set: None | Set[BaseBlock] = None) -> Mapping[BaseBlock, Set[BaseBlock]]:
+        """The internal helper method for making backwards adjacency list 
+        (block to block, from the end to the start) of the pipeline.
+        
+        Args:
+            feasible set: The set of the blocks, which will be used in the adjacency list. 
+            Other blocks will not be included. If 'None', all blocks of the pipeline will be used. 
+
+        Returns:
+            The backwards adjacency list containing blocks from the 'feasible set'.
+        """
         backward_aj_list: Mapping[BaseBlock, Set[BaseBlock]] = defaultdict(set)
         for inp_slot, out_slot in self.conn_dict.items():
             if feasible_set is None or \
@@ -99,7 +173,16 @@ class TopologicalExecutor(BaseExecutor, PainterMixin):
                 backward_aj_list[self.slot_to_block_map[inp_slot]].add(self.slot_to_block_map[out_slot])
         return backward_aj_list
 
-    def _get_forward_aj_list(self, feasible_set: Set[BaseBlock] = None) -> Mapping[BaseBlock, Set[BaseBlock]]:
+    def _get_forward_aj_list(self, feasible_set: None | Set[BaseBlock] = None) -> Mapping[BaseBlock, Set[BaseBlock]]:
+        """The internal helper method for making adjacency list (block to block) of the pipeline.
+        
+        Args:
+            feasible set: The set of the blocks, which will be used in the adjacency list. 
+            Other blocks will not be included. If 'None', all blocks of the pipeline will be used. 
+
+        Returns:
+            The adjacency list containing blocks from the 'feasible set'.
+        """
         forward_aj_list: Mapping[BaseBlock, Set[BaseBlock]] = defaultdict(set)
         for inp_slot, out_slot in self.conn_dict.items():
             if feasible_set is None or \
@@ -108,6 +191,24 @@ class TopologicalExecutor(BaseExecutor, PainterMixin):
         return forward_aj_list
 
     def __call__(self, input_values: Mapping[str, Data]) -> Mapping[str, Data]:
+        """The main method for the processing of the computational graph.
+        
+        Args:
+            input_values: The dictionary, containing the pipeline's inputs names as keys 
+            and coresponding to them 'Data' as values.
+        
+        Returns:
+            The dictionary, containing the pipeline's outputs names as keys 
+            and coresponding to them 'Data' as values.
+
+        Raises:
+            AssertionError: If there are some incompatibility between pipeline's inputs and user's ones.
+
+        Todo:
+            Think of the partial graph use: now the topology sort is performed using pipeline's inputs, 
+            not the inputs in 'input_values'.
+
+        """
         self.__check_inputs_concordance(input_values)
 
         output_blocks = [self.slot_to_block_map[slot] for slot in self.outputs.values()]
@@ -141,6 +242,20 @@ class TopologicalExecutor(BaseExecutor, PainterMixin):
         return result
 
     def draw(self, output_filename: str) -> None:
+        """Method for the computational graph drawing. This executor uses 
+        the graphviz library.
+
+        The solid black edges and nodes are the ones that will be used during calculations. 
+        The dashed will be skipped during the optimization. 
+        The red colored nodes mean inputs and outputs, the red colored edges signalize that
+        type of the block's input slot was misspecified: according to the stage metainformation 
+        the connection should be used, but the corresponding block won't be used because of the optimization.
+
+        Args:
+            output_filename: Path (containing the filename) where the output graphics file will be saved.
+            The filename determines the format, for example, figure.png will be rendered as a raster graphics file
+            and figure.pdf - as a vector one. The range of the formats depends on the cairo renderer: https://graphviz.org/docs/outputs/.
+        """
         output_blocks = [self.slot_to_block_map[slot] for slot in self.outputs.values()]
         backward_pass = self._dfs(self._get_backward_aj_list(), output_blocks)
 
