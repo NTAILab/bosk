cb99ddcbec62f9d52b8e3b99ae2a237cc55eb3b9
stasychbr <stasychbr@mail.ru>
2022-11-19 00:54:23 +0300

    [BOSK-35] Growing deep forest update

--- examples/deep_forests/growing/growing2.py
+++ examples/deep_forests/growing/growing2.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import List, Mapping, Optional, Set, Type, Any
+from typing import List, Mapping, Optional, Set, Type, Any, Sequence, Dict
+from dataclasses import dataclass, field
 from bosk import Data
 from bosk.executor.base import BaseExecutor
 from bosk.executor.handlers import SimpleExecutionStrategy, InputSlotStrategy
@@ -23,6 +24,19 @@ from collections import defaultdict
 
 
 class StepwisePipeline():
+    """Structure that provides possibility to build the pipeline
+    step by step. You select input blocks and output names of the final pipeline
+    and then merge new parts into the global result. 
+    
+    The main idea is to use same names of the inputs and outputs which should be
+    connected between steps.
+    
+    The common input blocks will try to connect with every added step.
+    
+    The global output slots will be chosen by the common output names and the
+    preference will be given to the last added output slot with the corresponding name.
+
+    """
     def __init__(self, input_blocks: Mapping[str, BaseBlock], outputs: Set[str]):
         for inp_block in input_blocks.values():
             assert len(inp_block.slots.inputs) == 1, "Input blocks must contain only 1 input slot"
@@ -33,9 +47,8 @@ class StepwisePipeline():
         self.cur_internal_conns: List[Connection] = []
         self.cur_inp_conns: Mapping[str, List[BlockInputSlot]] = defaultdict(list)
         self.cur_outputs: Mapping[str, BlockOutputSlot] = dict()
-        self.new_step: BasePipeline = None
 
-    def add_step(self, pipeline: BasePipeline, f_check_in_out: bool = True):
+    def merge_new_step(self, pipeline: BasePipeline, f_check_in_out: bool = True):
         if f_check_in_out:
             for inp in self.common_inputs:
                 if inp not in pipeline.inputs:
@@ -43,41 +56,20 @@ class StepwisePipeline():
             for out in self.common_outputs:
                 if out not in pipeline.outputs:
                     warnings.warn(f'Unable to find common output "{out}" in the new step')
-        self.new_step = pipeline # deepcopy?
-    
-    def get_proccessed_new_step(self):
-        assert self.new_step is not None, 'Firstly use add_step'
-        nodes = copy.copy(self.new_step.nodes)
-        conns = copy.copy(self.new_step.connections)
-        used_com_inputs = dict()
-        for inp_name, inp_block in self.common_inputs.items():
-            if inp_name in self.new_step.inputs:
-                nodes.append(inp_block)
-                out_slot = next(iter(inp_block.slots.outputs.values()))
-                conns.append(Connection(out_slot, self.new_step.inputs[inp_name]))
-                inp_slot = next(iter(inp_block.slots.inputs.values()))
-                used_com_inputs[inp_name] = inp_slot
-        inputs = copy.copy(self.new_step.inputs)
-        inputs.update(used_com_inputs)
-        return BasePipeline(nodes=nodes, connections=conns, inputs=inputs, outputs=self.new_step.outputs)
-
-    def merge_step(self):
-        assert self.new_step is not None, 'Firstly use add_step'
         for inp_name in self.common_inputs:
-            inp_slot = self.new_step.inputs.get(inp_name, None)
+            inp_slot = pipeline.inputs.get(inp_name, None)
             if inp_slot is not None:
                 self.cur_inp_conns[inp_name].append(inp_slot)
-        for inp_name, inp_slot in self.new_step.inputs.items():
+        for inp_name, inp_slot in pipeline.inputs.items():
             out_slot = self.cur_outputs.get(inp_name, None)
             if out_slot is not None:
                 self.cur_internal_conns.append(Connection(out_slot, inp_slot))
         for out_name in self.common_outputs:
-            out_slot = self.new_step.outputs.get(out_name, None)
+            out_slot = pipeline.outputs.get(out_name, None)
             if out_slot is not None:
                 self.cur_outputs[out_name] = out_slot
-        self.cur_internal_conns.extend(self.new_step.connections)
-        self.cur_nodes.extend(self.new_step.nodes)
-        self.new_step = None
+        self.cur_internal_conns.extend(pipeline.connections)
+        self.cur_nodes.extend(pipeline.nodes)
 
     def build_base_pipeline(self):
         inp_conns = []
@@ -90,31 +82,78 @@ class StepwisePipeline():
                         inputs=inputs, outputs=self.cur_outputs)
 
 
-class GrowingStrategy(ABC):
+class BaseGrowingStrategy(ABC):
+    """Class that defines the growing strategy of the deep forest.
+    It computes metrics in the :meth:`need_grow` method and decides
+    if it is needed to make further grow.
+    """
     @abstractmethod
-    def need_grow(self, test_data: Mapping[str, Data], test_labels: Mapping[str, Data]) -> bool:
+    def need_grow(self, labels: Optional[Mapping[str, Data]] = None,
+                transform_results: Optional[Mapping[str, Data]] = None,
+                fit_results: Optional[Mapping[str, Data]] = None) -> bool:
         ...
     
+    @abstractmethod
     def get_details(self) -> Mapping[str, Any]:
-        return {}
+        ...
 
 class LayerFactoryBase(ABC):
     @abstractmethod
     def get_new_layer(self) -> BasePipeline:
         ...
 
-class ROCAUCStrategy(GrowingStrategy):
+@dataclass
+class EarlyStoppingStrategy(BaseGrowingStrategy):
+    max_iter_num: int 
+    patience: int # todo
+    _cur_iter: int = field(default=0, init=False)
+
+    def need_grow(self, labels: Optional[Mapping[str, Data]] = None,
+                transform_results: Optional[Mapping[str, Data]] = None,
+                fit_results: Optional[Mapping[str, Data]] = None) -> bool:
+        self._cur_iter += 1
+        return self._cur_iter < self.max_iter_num
+    
+    def get_details(self) -> Mapping[str, Any]:
+        return {'iterations remain': self.max_iter_num - self._cur_iter}
+
+class ROCAUCStrategy(BaseGrowingStrategy):
     def __init__(self):
         self.last_score = 0
 
-    def need_grow(self, test_data: Mapping[str, Data], test_labels: Mapping[str, Data]) -> bool:
-        current_score = roc_auc_score(test_labels['labels'], test_data['probas'][:, 1])
+    def need_grow(self, labels: Optional[Mapping[str, Data]] = None,
+                transform_results: Optional[Mapping[str, Data]] = None,
+                fit_results: Optional[Mapping[str, Data]] = None) -> bool:
+        assert labels is not None
+        if transform_results is None:
+            return True
+        current_score = roc_auc_score(labels['labels'], transform_results['probas'][:, 1])
         result = self.last_score < current_score
         self.last_score = current_score
         return result
     
     def get_details(self) -> Mapping[str, Any]:
         return {'roc-auc test score': self.last_score}
+
+class ComplexGrowingStrategy(BaseGrowingStrategy):
+    def __init__(self, strategies: Sequence[BaseGrowingStrategy]) -> None:
+        self.strat_seq = copy.copy(strategies)
+    
+    def need_grow(self, labels: Optional[Mapping[str, Data]] = None,
+                transform_results: Optional[Mapping[str, Data]] = None,
+                fit_results: Optional[Mapping[str, Data]] = None) -> bool:
+        return all(map(lambda strat: strat.need_grow(labels, transform_results, fit_results), self.strat_seq))
+    
+    def get_details(self) -> Mapping[str, Any]:
+        res = dict()
+        for i, strat in enumerate(self.strat_seq):
+            cur_details = strat.get_details()
+            for key, val in cur_details.items():
+                if key in res:
+                    res[f'{key}_{i + 1}'] = val
+                else:
+                    res[key] = val
+        return res
 
 class SimpleDFLayerFactory(LayerFactoryBase):
     def __init__(self):
@@ -154,21 +193,17 @@ class FitCallback(ABC):
     def __call__(self, df_fit_output: Mapping[str, Data]) -> None:
         ...
 
-# this test is specically made to test unnecessary internal output 
+# this test is specically made to test unnecessary internal output
 class ROCAUCCallback(FitCallback):
     def __call__(self, df_fit_output: Mapping[str, Data]) -> None:
         print('Fit roc-auc score:', df_fit_output['roc-auc'])
 
-# Need to change the name
-class EarlyStoppingManager():
-
-    def __init__(self, max_depth: int, pipeline: StepwisePipeline,
-                layer_factory: LayerFactoryBase, growing_strategy: GrowingStrategy,
+class SimpleGrowingFitter():
+    def __init__(self, pipeline: StepwisePipeline,
+                layer_factory: LayerFactoryBase, growing_strategy: BaseGrowingStrategy,
                 executor_cls: Type[BaseExecutor], exec_kw = {},
                 fit_callback: Optional[FitCallback] = None):
         self.growing_pipeline = pipeline
-        self.max_depth = max_depth
-        self.patience = 0 # todo
         self.layer_factory = layer_factory
         self.strategy = growing_strategy
         self.exec_cls = executor_cls
@@ -176,37 +211,44 @@ class EarlyStoppingManager():
         self.is_fitted = False
         self.fit_callback = fit_callback
 
-    def fit(self, train_data: Mapping[str, Data], val_data: Mapping[str, Data]) -> None:
+    def _print_log(self):
+        grow_details = self.strategy.get_details()
+        print('Testing log:')
+        for key, val in grow_details.items():
+            print(f'\t{key}: {val}')
+
+    def fit(self, train_data: Dict[str, Data], val_data: Dict[str, Data], labels: Dict[str, Data]) -> None:
         input_fit_data = copy.copy(train_data)
-        input_val_data = copy.copy(val_data)
-        for i in range(1, self.max_depth + 1):
+        input_test_data = copy.copy(val_data)
+        fit_output = None
+        test_output = None
+        i = 1
+        print('Beginning the fit process')
+        while True:
             print(f'--- Iteration {i} ---')
             new_layer = self.layer_factory.get_new_layer()
-            self.growing_pipeline.add_step(new_layer)
-            new_df = self.growing_pipeline.get_proccessed_new_step()
-            fit_exec = self.exec_cls(new_df,
-                InputSlotStrategy(Stage.FIT), 
+            fit_exec = self.exec_cls(new_layer,
+                InputSlotStrategy(Stage.FIT),
                 SimpleExecutionStrategy(Stage.FIT),
                 stage=Stage.FIT, **self.exec_kw)
             fit_output = fit_exec(input_fit_data)
             if self.fit_callback is not None:
                 self.fit_callback(fit_output)
-            tf_exec = self.exec_cls(new_df, 
+            tf_exec = self.exec_cls(new_layer,
                 InputSlotStrategy(Stage.TRANSFORM),
                 SimpleExecutionStrategy(Stage.TRANSFORM),
                 stage=Stage.TRANSFORM, **self.exec_kw)
-            test_output = tf_exec(input_val_data)
-            f_need_grow = self.strategy.need_grow(test_output, input_val_data)
-            grow_details = self.strategy.get_details()
-            print('Testing log:')
-            for key, val in grow_details.items():
-                print(f'\t{key}: {val}')
-            if not f_need_grow:
-                print('Training is interrupted')
+            test_output = tf_exec(input_test_data)
+            f_need_continue = self.strategy.need_grow(labels, test_output, fit_output)
+            self._print_log()
+            if not f_need_continue:
+                print('Training is stopped')
                 break
-            self.growing_pipeline.merge_step()
+            print('Layer is added')
+            self.growing_pipeline.merge_new_step(new_layer)
             input_fit_data.update(fit_output)
-            input_val_data.update(test_output)
+            input_test_data.update(test_output)
+            i += 1
         self.is_fitted = True
         
     def get_pipeline(self):
@@ -236,10 +278,13 @@ def main():
         },
         {'probas', 'labels'}
     )
-    growing_manager = EarlyStoppingManager(5, growing_pipeline, SimpleDFLayerFactory(), ROCAUCStrategy(),
+    early_stop_strat = EarlyStoppingStrategy(5, 0)
+    roc_auc_strat = ROCAUCStrategy()
+    final_strat = ComplexGrowingStrategy([early_stop_strat, roc_auc_strat])
+    growing_manager = SimpleGrowingFitter(growing_pipeline, SimpleDFLayerFactory(), final_strat,
         TopologicalExecutor, fit_callback=ROCAUCCallback())
 
-    growing_manager.fit({'X': train_X, 'y': train_y}, {'X': val_X, 'labels': val_y})
+    growing_manager.fit({'X': train_X, 'y': train_y}, {'X': val_X}, {'labels': val_y})
     tf_exec = growing_manager.get_transform_executor()
     output = tf_exec({'X': test_X})
 
