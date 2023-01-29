54d93c3a79011542bc6fcbe81b0ecce1422f2811
AndreyAgeev <andreyageev1@mail.ru>
2023-01-29 15:40:39 +0300

    [BOSK-61] feat: Added draft for GPU and CPU Data classes

--- bosk/data.py
+++ bosk/data.py
@@ -4,3 +4,15 @@ from typing import Any
 Data = Any
 """Type for data which will be transferred between blocks.
 """
+
+
+class BaseData:
+    ...  # common parameters
+
+
+class CPUData(BaseData):
+    ...  # cpu-specific parameters
+
+
+class GPUData(BaseData):
+    ...  # cpu-specific parameters
