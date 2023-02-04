1219204d824ff2863c1854ee29c99d803b2a3bd6
Stasychbr <stasychbr@mail.ru>
2023-02-04 17:30:08 +0300

    [BOSK-49] Added pytest config

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -3,10 +3,11 @@ from bosk.executor.descriptor import HandlingDescriptor
 from bosk.stages import Stage
 from .pipeline_getter import BasePipelineGetter
 from .pipelines import *
+import logging
 
 def test_pipelines():
     pl_getter_cls = BasePipelineGetter.__subclasses__()
-    print('Following classes for the test were found:', [cls.__name__ for cls in pl_getter_cls])
+    logging.info('Following classes for the test were found: ' + format([cls.__name__ for cls in pl_getter_cls]))
     for plg_cls in pl_getter_cls:
         plg = plg_cls()
         pipeline = plg.get_pipeline()
