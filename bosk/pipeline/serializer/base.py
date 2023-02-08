f5627c83a367019593f28218834c5d2cec408632
Andrei Konstantinov <andrue.konst@gmail.com>
2023-02-08 21:54:54 +0300

    [BOSK-53] Update base serializer: add docs

--- bosk/pipeline/serializer/base.py
+++ bosk/pipeline/serializer/base.py
@@ -3,10 +3,27 @@ from ..base import BasePipeline
 
 
 class BaseSerializer(ABC):
+    """Base Serializer.
+
+    Can be used for the whole pipeline serialization.
+    """
     @abstractmethod
     def dump(self, pipeline: BasePipeline, out_file):
+        """Serialize and dump the pipeline to the file.
+
+        Args:
+            pipeline: The pipeline to serialize.
+            out_file: Output file or stream.
+
+        """
         ...
 
     @abstractmethod
     def load(self, in_file) -> BasePipeline:
+        """Load and deserialize a pipeline from the file.
+
+        Args:
+            in_file: Input file or stream.
+
+        """
         ...
