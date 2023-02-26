[BOSK-63] fix: added GPU information to the meta, added data match checks (all data is either GPU or CPU within a block), fixed GPU or CPU calculation depending on the data, added blocks with GPU calculation, added an example

bosk/block/zoo/data_conversion/data_conversion.py
@@ -4,7 +4,7 @@ import jax.numpy as jnp
 import numpy as np
 
 from bosk.block import BaseBlock, BlockInputData, TransformOutputData
-from bosk.block.meta import make_simple_meta
+from bosk.block.meta import make_simple_meta, BlockExecutionProperties
 from bosk.data import GPUData, CPUData
 
 
@@ -12,25 +12,11 @@ class ConcatBlock(BaseBlock):
     meta = None
 
     def __init__(self, input_names: List[str], axis: int = -1):
-        self.meta = make_simple_meta(input_names, ['output'])
+        self.meta = make_simple_meta(input_names, ['output'],
+                                     execution_props=BlockExecutionProperties(cpu=True, gpu=True))
         super().__init__()
         self.axis = axis
         self.ordered_input_names = None
-
-    def fit_gpu(self, inputs: BlockInputData) -> 'ConcatBlock':
-        return self.fit(inputs)
-
-    def transform_gpu(self, inputs: BlockInputData) -> TransformOutputData:
-        assert self.ordered_input_names is not None
-        ordered_inputs = []
-        for name in self.ordered_input_names:
-            if isinstance(inputs[name], CPUData):
-                ordered_inputs.append(inputs[name].to_gpu().data)  # or raise Exception?
-            else:
-                ordered_inputs.append(inputs[name].data)
-        ordered_inputs = tuple(ordered_inputs)
-        concatenated = jnp.concatenate(ordered_inputs, axis=self.axis)
-        return {'output': GPUData(concatenated)}
 
     def fit(self, inputs: BlockInputData) -> 'ConcatBlock':
         self.ordered_input_names = list(inputs.keys())
@@ -39,14 +25,22 @@ class ConcatBlock(BaseBlock):
 
     def transform(self, inputs: BlockInputData) -> TransformOutputData:
         assert self.ordered_input_names is not None
+        input_type = type(next(iter(inputs.values())))
         ordered_inputs = []
         for name in self.ordered_input_names:
-            if isinstance(inputs[name], GPUData):
-                ordered_inputs.append(inputs[name].to_cpu().data)  # or raise Exception?
-            else:
+            if isinstance(inputs[name], input_type):
                 ordered_inputs.append(inputs[name].data)
+            else:
+                x = 0
+                raise ValueError("All inputs must be of the same type (CPUData or GPUData)")
         ordered_inputs = tuple(ordered_inputs)
-        concatenated = np.concatenate(ordered_inputs, axis=self.axis)
+        if input_type == CPUData:
+            concatenated = CPUData(np.concatenate(ordered_inputs, axis=self.axis))
+        elif input_type == GPUData:
+            concatenated = GPUData(jnp.concatenate(ordered_inputs, axis=self.axis))
+        else:
+            raise ValueError(f"Unexpected input type: {input_type}")
+
         return {'output': concatenated}
 
 
@@ -54,27 +48,11 @@ class StackBlock(BaseBlock):
     meta = None
 
     def __init__(self, input_names: List[str], axis: int = -1):
-        self.meta = make_simple_meta(input_names, ['output'])
+        self.meta = make_simple_meta(input_names, ['output'],
+                                     execution_props=BlockExecutionProperties(cpu=True, gpu=True))
         super().__init__()
         self.axis = axis
         self.ordered_input_names = None
-
-    def fit_gpu(self, inputs: BlockInputData) -> 'StackBlock':
-        self.ordered_input_names = list(inputs.keys())
-        self.ordered_input_names.sort()
-        return self
-
-    def transform_gpu(self, inputs: BlockInputData) -> TransformOutputData:
-        assert self.ordered_input_names is not None
-        ordered_inputs = []
-        for name in self.ordered_input_names:
-            if isinstance(inputs[name], CPUData):
-                ordered_inputs.append(inputs[name].to_gpu().data)  # or raise Exception?
-            else:
-                ordered_inputs.append(inputs[name].data)
-        ordered_inputs = tuple(ordered_inputs)
-        stacked = jnp.stack(ordered_inputs, axis=self.axis)
-        return {'output': GPUData(stacked)}
 
     def fit(self, inputs: BlockInputData) -> 'StackBlock':
         self.ordered_input_names = list(inputs.keys())
@@ -83,72 +61,62 @@ class StackBlock(BaseBlock):
 
     def transform(self, inputs: BlockInputData) -> TransformOutputData:
         assert self.ordered_input_names is not None
+        input_type = type(next(iter(inputs.values())))
         ordered_inputs = []
         for name in self.ordered_input_names:
-            if isinstance(inputs[name], GPUData):
-                ordered_inputs.append(inputs[name].to_cpu().data)  # or raise Exception?
-            else:
+            if isinstance(inputs[name], input_type):
                 ordered_inputs.append(inputs[name].data)
+            else:
+                raise ValueError("All inputs must be of the same type (CPUData or GPUData)")
         ordered_inputs = tuple(ordered_inputs)
-        stacked = np.stack(ordered_inputs, axis=self.axis)
-        return {'output': CPUData(stacked)}
+        if input_type == CPUData:
+            stacked = CPUData(np.stack(ordered_inputs, axis=self.axis))
+        elif input_type == GPUData:
+            stacked = GPUData(jnp.stack(ordered_inputs, axis=self.axis))
+        else:
+            raise ValueError(f"Unexpected input type: {input_type}")
+        return {'output': stacked}
 
 
 class AverageBlock(BaseBlock):
-    meta = make_simple_meta(['X'], ['output'])
+    meta = make_simple_meta(['X'], ['output'], execution_props=BlockExecutionProperties(cpu=True, gpu=True))
 
     def __init__(self, axis: int = -1):
         super().__init__()
         self.axis = axis
-
-    def fit_gpu(self, inputs: BlockInputData) -> 'AverageBlock':
-        return self
-
-    def transform_gpu(self, inputs: BlockInputData) -> TransformOutputData:
-        assert 'X' in inputs
-        if isinstance(inputs['X'], CPUData):
-            averaged = inputs['X'].to_gpu().data.mean(axis=self.axis)
-        else:
-            averaged = inputs['X'].data.mean(axis=self.axis)
-        return {'output': GPUData(averaged)}
 
     def fit(self, _inputs: BlockInputData) -> 'AverageBlock':
         return self
 
     def transform(self, inputs: BlockInputData) -> TransformOutputData:
         assert 'X' in inputs
-        if isinstance(inputs['X'], GPUData):
-            averaged = inputs['X'].to_cpu().data.mean(axis=self.axis)
+        input_type = type(inputs['X'])
+        if input_type not in [CPUData, GPUData]:
+            raise TypeError("All inputs must be of type: CPUData or GPUData.")
+        averaged = inputs['X'].data.mean(axis=self.axis)
+        if input_type == CPUData:
+            return {'output': CPUData(averaged)}
         else:
-            averaged = inputs['X'].data.mean(axis=self.axis)
-        return {'output': CPUData(averaged)}
+            return {'output': GPUData(averaged)}
 
 
 class ArgmaxBlock(BaseBlock):
-    meta = make_simple_meta(['X'], ['output'])
+    meta = make_simple_meta(['X'], ['output'], execution_props=BlockExecutionProperties(cpu=True, gpu=True))
 
     def __init__(self, axis: int = -1):
         super().__init__()
         self.axis = axis
-
-    def fit_gpu(self, inputs: BlockInputData) -> 'ArgmaxBlock':
-        return self
-
-    def transform_gpu(self, inputs: BlockInputData) -> TransformOutputData:
-        assert 'X' in inputs
-        if isinstance(inputs['X'], CPUData):
-            ids = inputs['X'].to_gpu().data.argmax(axis=self.axis)
-        else:
-            ids = inputs['X'].data.argmax(axis=self.axis)
-        return {'output': GPUData(ids)}
 
     def fit(self, _inputs: BlockInputData) -> 'ArgmaxBlock':
         return self
 
     def transform(self, inputs: BlockInputData) -> TransformOutputData:
         assert 'X' in inputs
-        if isinstance(inputs['X'], GPUData):
-            ids = inputs['X'].to_cpu().data.argmax(axis=self.axis)
+        input_type = type(inputs['X'])
+        if input_type not in [CPUData, GPUData]:
+            raise TypeError("All inputs must be of type: CPUData or GPUData.")
+        ids = inputs['X'].data.argmax(axis=self.axis)
+        if input_type == CPUData:
+            return {'output': CPUData(ids)}
         else:
-            ids = inputs['X'].data.argmax(axis=self.axis)
-        return {'output': CPUData(ids)}
+            return {'output': GPUData(ids)}

examples/deep_forests/gpu/gpu_simple.py
@@ -26,8 +26,6 @@ def make_deep_forest_functional_cpu(executor, **ex_kw):
     b = FunctionalPipelineBuilder()
     X, y = b.Input()(), b.TargetInput()()
     X = b.MoveTo("CPU")(X=X)
-    # rf_1 = b.RFC(random_state=42)(X=X, y=y)
-    # et_1 = b.ETC(random_state=42)(X=X, y=y)
     concat_1 = b.Concat(['X', 'X_1', 'X_2'])(X=X, X_1=X, X_2=X)
 
     fit_executor = executor(
@@ -57,8 +55,6 @@ def make_deep_forest_functional_gpu(executor, **ex_kw):
     b = FunctionalPipelineBuilder()
     X, y = b.Input()(), b.TargetInput()()
     X = b.MoveTo("GPU")(X=X)
-    # rf_1 = b.RFC(random_state=42)(X=X, y=y)
-    # et_1 = b.ETC(random_state=42)(X=X, y=y)
     concat_1 = b.Concat(['X', 'X_1', 'X_2'])(X=X, X_1=X, X_2=X)
 
     fit_executor = executor(
@@ -90,10 +86,10 @@ def run_cpu():
 
     all_X, all_y = make_moons(n_samples=1000000, noise=0.5, random_state=42)
     train_X, test_X, train_y, test_y = train_test_split(all_X, all_y, test_size=0.2, random_state=42)
-    fit_result = fit_executor({'X': CPUData(train_X), 'y': CPUData(train_y)})
+    fit_executor({'X': CPUData(train_X), 'y': CPUData(train_y)})
     print("Fit successful")
-    train_result = transform_executor({'X': CPUData(train_X)})
-    test_result = transform_executor({'X': CPUData(test_X)})
+    transform_executor({'X': CPUData(train_X)})
+    transform_executor({'X': CPUData(test_X)})
 
 
 def run_gpu():
@@ -104,16 +100,168 @@ def run_gpu():
     train_X, test_X, train_y, test_y = train_test_split(all_X, all_y, test_size=0.2, random_state=42)
     fit_result = fit_executor({'X': GPUData(train_X), 'y': GPUData(train_y)})
     print("Fit successful")
+    transform_executor({'X': GPUData(train_X)})
+    transform_executor({'X': GPUData(test_X)})
+
+
+def make_deep_forest_functional_advanced_cpu(executor, **ex_kw):
+    b = FunctionalPipelineBuilder()
+    X, y = b.Input()(), b.TargetInput()()
+    rf_1 = b.RFC(random_state=42)(X=X, y=y)
+    et_1 = b.ETC(random_state=42)(X=X, y=y)
+    # rf_1 = b.MoveTo("CPU")(X=rf_1)
+    # et_1 = b.MoveTo("CPU")(X=et_1)
+    concat_1 = b.Concat(['X', 'rf_1', 'et_1'])(X=X, rf_1=rf_1, et_1=et_1)
+    rf_2 = b.RFC(random_state=42)(X=concat_1, y=y)
+    et_2 = b.ETC(random_state=42)(X=concat_1, y=y)
+    concat_2 = b.Concat(['X', 'rf_2', 'et_2'])(X=X, rf_2=rf_2, et_2=et_2)
+    rf_3 = b.RFC(random_state=42)(X=concat_2, y=y)
+    et_3 = b.ETC(random_state=42)(X=concat_2, y=y)
+    stack_3 = b.Stack(['rf_3', 'et_3'], axis=1)(rf_3=rf_3, et_3=et_3)
+    average_3 = b.Average(axis=1)(X=stack_3)
+    argmax_3 = b.Argmax(axis=1)(X=average_3)
+
+    rf_1_roc_auc = b.RocAuc()(gt_y=y, pred_probas=rf_1)
+    roc_auc = b.RocAuc()(gt_y=y, pred_probas=average_3)
+
+    fit_executor = executor(
+        b.build(
+            {'X': X, 'y': y},
+            {'probas': average_3, 'rf_1_roc-auc': rf_1_roc_auc, 'roc-auc': roc_auc}
+        ),
+        HandlingDescriptor.from_classes(Stage.FIT),
+        inputs=['X', 'y'],
+        outputs=['probas', 'rf_1_roc-auc', 'roc-auc'],
+        **ex_kw
+    )
+    transform_executor = executor(
+        b.build(
+            {'X': X, 'y': y},
+            {'probas': average_3, 'labels': argmax_3}
+        ),
+        HandlingDescriptor.from_classes(Stage.TRANSFORM),
+        inputs=['X'],
+        outputs=['probas', 'labels'],
+        **ex_kw
+    )
+    return fit_executor, transform_executor
+
+
+def make_deep_forest_functional_advanced_gpu(executor, **ex_kw):
+    b = FunctionalPipelineBuilder()
+    X, y = b.Input()(), b.TargetInput()()
+    rf_1 = b.RFC(random_state=42)(X=X, y=y)
+    et_1 = b.ETC(random_state=42)(X=X, y=y)
+    rf_1 = b.MoveTo("GPU")(X=rf_1)
+    et_1 = b.MoveTo("GPU")(X=et_1)
+    concat_1 = b.Concat(['X', 'rf_1', 'et_1'])(X=X, rf_1=rf_1, et_1=et_1)
+    rf_2 = b.RFC(random_state=42)(X=concat_1, y=y)
+    et_2 = b.ETC(random_state=42)(X=concat_1, y=y)
+    rf_2 = b.MoveTo("GPU")(X=rf_2)
+    et_2 = b.MoveTo("GPU")(X=et_2)
+    concat_2 = b.Concat(['X', 'rf_2', 'et_2'])(X=X, rf_2=rf_2, et_2=et_2)
+    rf_3 = b.RFC(random_state=42)(X=concat_2, y=y)
+    et_3 = b.ETC(random_state=42)(X=concat_2, y=y)
+    rf_3 = b.MoveTo("GPU")(X=rf_3)
+    et_3 = b.MoveTo("GPU")(X=et_3)
+    stack_3 = b.Stack(['rf_3', 'et_3'], axis=1)(rf_3=rf_3, et_3=et_3)
+    average_3 = b.Average(axis=1)(X=stack_3)
+    argmax_3 = b.Argmax(axis=1)(X=average_3)
+
+    rf_1_roc_auc = b.RocAuc()(gt_y=y, pred_probas=rf_1)
+    roc_auc = b.RocAuc()(gt_y=y, pred_probas=average_3)
+
+    fit_executor = executor(
+        b.build(
+            {'X': X, 'y': y},
+            {'probas': average_3, 'rf_1_roc-auc': rf_1_roc_auc, 'roc-auc': roc_auc}
+        ),
+        HandlingDescriptor.from_classes(Stage.FIT),
+        inputs=['X', 'y'],
+        outputs=['probas', 'rf_1_roc-auc', 'roc-auc'],
+        **ex_kw
+    )
+    transform_executor = executor(
+        b.build(
+            {'X': X, 'y': y},
+            {'probas': average_3, 'labels': argmax_3}
+        ),
+        HandlingDescriptor.from_classes(Stage.TRANSFORM),
+        inputs=['X'],
+        outputs=['probas', 'labels'],
+        **ex_kw
+    )
+    return fit_executor, transform_executor
+
+
+def run_cpu_advanced():
+    executor_class = RecursiveExecutor
+    fit_executor, transform_executor = make_deep_forest_functional_advanced_cpu(executor_class)
+
+    all_X, all_y = make_moons(noise=0.5, random_state=42)
+    train_X, test_X, train_y, test_y = train_test_split(all_X, all_y, test_size=0.2, random_state=42)
+    fit_result = fit_executor({'X': CPUData(train_X), 'y': CPUData(train_y)})
+    print("Fit successful")
+    train_result = transform_executor({'X': CPUData(train_X)})
+    print("Fit probas == probas on train:", np.allclose(fit_result['probas'].data, train_result['probas'].data))
+    test_result = transform_executor({'X': CPUData(test_X)})
+    print(train_result.keys())
+    print("Train ROC-AUC:", roc_auc_score(train_y.data, train_result['probas'].data[:, 1]))
+    print(
+        "Train ROC-AUC calculated by fit_executor:",
+        fit_result['roc-auc']
+    )
+    print(
+        "Train ROC-AUC for RF_1:",
+        fit_result['rf_1_roc-auc']
+    )
+    print("Test ROC-AUC:", roc_auc_score(test_y.data, test_result['probas'].data[:, 1]))
+
+
+def run_gpu_advanced():
+    executor_class = RecursiveExecutor
+    fit_executor, transform_executor = make_deep_forest_functional_advanced_gpu(executor_class)
+
+    all_X, all_y = make_moons(noise=0.5, random_state=42)
+    train_X, test_X, train_y, test_y = train_test_split(all_X, all_y, test_size=0.2, random_state=42)
+    fit_result = fit_executor({'X': GPUData(train_X), 'y': GPUData(train_y)})
+    print("Fit successful")
     train_result = transform_executor({'X': GPUData(train_X)})
+    print("Fit probas == probas on train:", np.allclose(fit_result['probas'].data, train_result['probas'].data))
     test_result = transform_executor({'X': GPUData(test_X)})
+    print(train_result.keys())
+    print("Train ROC-AUC:", roc_auc_score(train_y.data, train_result['probas'].data[:, 1]))
+    print(
+        "Train ROC-AUC calculated by fit_executor:",
+        fit_result['roc-auc']
+    )
+    print(
+        "Train ROC-AUC for RF_1:",
+        fit_result['rf_1_roc-auc']
+    )
+    print("Test ROC-AUC:", roc_auc_score(test_y.data, test_result['probas'].data[:, 1]))
 
 
 if __name__ == "__main__":
-    # run_cpu()
     import time
 
-    start_time = time.time() #0.2692
+    start_time = time.time()
+    run_cpu()
+    end_time = time.time()
+    print(f"Time CPU execution: {end_time - start_time} sec")
+
+    start_time = time.time()
     run_gpu()
     end_time = time.time()
+    print(f"Time GPU execution: {end_time - start_time} sec")
 
-    print(f"Time execution: {end_time - start_time} sec")
+    start_time = time.time()
+    run_cpu_advanced()
+    end_time = time.time()
+    print(f"Time CPU execution: {end_time - start_time} sec")
+
+    start_time = time.time()
+    run_gpu_advanced()  # more block - slower execution
+    end_time = time.time()
+    print(f"Time GPU execution: {end_time - start_time} sec")
+