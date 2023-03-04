99753309dca1405b34e877477dc8cc4d1a883e3a
stasychbr <stasychbr@mail.ru>
2023-03-04 15:40:12 +0300

    [BOSK-60] Added random_state metainfo for auto_blocks

--- bosk/block/zoo/models/regression/regression_models.py
+++ bosk/block/zoo/models/regression/regression_models.py
@@ -22,7 +22,7 @@ class ETRBlock(ExtraTreesRegressor):
         return CPUData(self.predict(X))
 
 
-@auto_block(execution_props=BlockExecutionProperties())
+@auto_block(execution_props=BlockExecutionProperties(), random_state_field='random_seed_')
 class CatBoostRegressorBlock(CatBoostRegressor):
     def transform(self, X):
         return CPUData(self.predict(X))
