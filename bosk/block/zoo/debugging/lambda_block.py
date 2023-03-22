ce399813bedaea5c7808240d16daab9d6ef24b65
Andrei Konstantinov <andrue.konst@gmail.com>
2023-03-22 18:57:05 +0300

    [BOSK-77] Add TransformLambdaBlock

--- bosk/block/zoo/debugging/lambda_block.py
+++ bosk/block/zoo/debugging/lambda_block.py
@@ -38,3 +38,39 @@ class FitLambdaBlock(BaseBlock):
 
     def transform(self, inputs: BlockInputData) -> TransformOutputData:
         return inputs
+
+
+class TransformLambdaBlock(BaseBlock):
+    """Transform Lambda Block that executes some function at the fit stage
+    and bypasses input ad the transform stage.
+    """
+
+    meta = None
+
+    def __init__(self, function: Callable, inputs: Sequence[str]):
+        self.meta = BlockMeta(
+            inputs=[
+                InputSlotMeta(
+                    name=inp,
+                    stages=Stages(transform=True, transform_on_fit=True),
+                )
+                for inp in inputs
+            ],
+            outputs=[
+                OutputSlotMeta(
+                    name=inp,
+                )
+                for inp in inputs
+            ],
+            execution_props=BlockExecutionProperties(plain=True),
+        )
+        self.function = function
+        super().__init__()
+
+    def fit(self, _inputs: BlockInputData) -> 'FitLambdaBlock':
+        return self
+
+    def transform(self, inputs: BlockInputData) -> TransformOutputData:
+        self.function(inputs)
+        return inputs
+
