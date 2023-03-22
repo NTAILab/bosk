590e301840938f762139bb14c70b8690c0eb1c09
Andrei Konstantinov <andrue.konst@gmail.com>
2023-03-22 19:11:37 +0300

    [BOSK-77] Update PoolingBlock: separate convolution implementation

--- bosk/block/zoo/multi_grained_scanning/pooling.py
+++ bosk/block/zoo/multi_grained_scanning/pooling.py
@@ -4,28 +4,17 @@ from functools import partial
 from ...base import BaseBlock, BlockInputData, TransformOutputData
 from ...meta import make_simple_meta, BlockExecutionProperties
 from ....data import CPUData, GPUData
+from ._convolution_helpers import (
+    _PoolingIndices,
+    _ConvolutionParams,
+    _ConvolutionHelper,
+)
 
 
 AGGREGATION_FUNCTIONS = {
     'max': partial(np.max, axis=-1),
     'mean': partial(np.mean, axis=-1),
 }
-
-
-class PoolingIndices(NamedTuple):
-    """
-    Attributes:
-        xs_shape: Input shape.
-        full_index_tuple: Tuple of indices of each pixel for each group (corner position).
-        n_corners: Number of corners.
-        n_kernel_points: Number of points inside kernel.
-        pooled_shape: Pooling result shape (excluding the n_sample dimension).
-    """
-    xs_shape: Tuple[int]
-    full_index_tuple: Tuple[np.ndarray]
-    n_corners: int
-    n_kernel_points: int
-    pooled_shape: Tuple[int]
 
 
 class PoolingBlock(BaseBlock):
@@ -60,13 +49,16 @@ class PoolingBlock(BaseBlock):
 
         """
         super().__init__()
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.dilation = dilation
-        self.padding = padding
+        self.params = _ConvolutionParams(
+            kernel_size=kernel_size,
+            stride=stride,
+            dilation=dilation,
+            padding=padding,
+        )
         self.aggregation = aggregation
         self.chunk_size = chunk_size
-        self.pooling_indices_: Optional[PoolingBlock.PoolingIndices] = None
+        self.pooling_indices_: Optional[_PoolingIndices] = None
+        self.helper_ = _ConvolutionHelper(self.params)
 
     def fit(self, _inputs: BlockInputData) -> 'PoolingBlock':
         return self
@@ -84,90 +76,17 @@ class PoolingBlock(BaseBlock):
             return AGGREGATION_FUNCTIONS[self.aggregation](grouped_data)
         raise ValueError(f'Wrong aggregation function: {self.aggregation!r}')
 
-    def __prepare_kernel(self, n_spatial_dims):
-        if isinstance(self.kernel_size, tuple):
-            kernel_size = self.kernel_size
-        else:
-            kernel_size = (self.kernel_size,) * n_spatial_dims
-        kernel_ids = np.stack(np.meshgrid(*[np.arange(k) for k in kernel_size], indexing='ij'), axis=0)
-        # kernel_ids shape: (n_spatial_dims, k_1, ..., k_nsd)
-        if self.dilation > 1:
-            kernel_ids = kernel_ids[:, np.all(kernel_ids % self.dilation == 0, axis=0)]
-        kernel_ids = kernel_ids.reshape((kernel_ids.shape[0], -1))
-        return kernel_size, kernel_ids
-
-    def __prepare_corner(self, spatial_dims, kernel_size):
-        n_spatial_dims = len(spatial_dims)
-        if self.stride is None:
-            stride = kernel_size
-        elif isinstance(self.stride, tuple):
-            stride = self.stride
-        else:
-            stride = (self.stride,) * n_spatial_dims
-        corner_ids = np.stack(
-            np.meshgrid(*[
-                np.arange(
-                    0,
-                    (
-                        (spatial_dims[i] // s) * s if s >= kernel_size[i]
-                        else ((spatial_dims[i] - kernel_size[i] + 1) // s) * s
-                    ),
-                    s
-                )
-                for i, s in enumerate(stride)
-            ], indexing='ij'),
-            axis=0
-        )
-        return corner_ids
-
     def __prepare_pooling_indices(self, xs_shape):
         if self.pooling_indices_ is not None and xs_shape == self.pooling_indices_.xs_shape:
             return self.pooling_indices_
 
-        _, n_channels, *spatial_dims = xs_shape
-        n_spatial_dims = len(spatial_dims)
-        kernel_size, kernel_ids = self.__prepare_kernel(n_spatial_dims)
-        corner_ids = self.__prepare_corner(spatial_dims, kernel_size)
-        pooled_shape = corner_ids.shape[1:]
-        corner_ids = corner_ids.reshape((corner_ids.shape[0], -1))
-        # corner_ids shape: (n_spatial_dims, s_1 * ... * s_nsd)
-        all_ids = corner_ids[:, :, np.newaxis] + kernel_ids[:, np.newaxis, :]
-        all_ids = all_ids.reshape((n_spatial_dims, -1))
-        # all_ids shape: (n_spatial_dims, n_positions)
-        full_index_tuple = tuple(
-            np.concatenate((
-                np.arange(all_ids.shape[1] * n_channels)[np.newaxis] // all_ids.shape[1],
-                np.repeat(all_ids, n_channels, axis=1)
-            ), axis=0)
-        )
-        self.pooling_indices_ = PoolingIndices(
-            xs_shape,
-            full_index_tuple,
-            corner_ids.shape[1],
-            kernel_ids.shape[1],
-            pooled_shape
-        )
+        self.pooling_indices_ = self.helper_.prepare_pooling_indices(xs_shape)
         return self.pooling_indices_
-
-    def __pad(self, xs: np.ndarray) -> np.ndarray:
-        """Add zero padding to the input array.
-        """
-        n_spatial_dims = xs.ndim - 2
-        if isinstance(self.padding, tuple):
-            padding_size = self.padding
-        else:
-            padding_size = ((self.padding, self.padding),) * n_spatial_dims
-        return np.pad(
-            xs,
-            pad_width=((0, 0), (0, 0), *padding_size),
-            mode='edge',
-        )
-
 
     def __chunk_pooling(self, xs: np.ndarray) -> np.ndarray:
         n_samples, n_channels, *_ = xs.shape
-        if self.padding is not None:
-            xs = self.__pad(xs)
+        if self.params.padding is not None:
+            xs = self.helper_.pad(xs)
         pooling_indices = self.__prepare_pooling_indices(xs.shape)
         result = []
         for i in range(n_samples):
