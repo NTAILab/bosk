445e9120425efad9a7b11763c23d8e045e267a17
Andrei Konstantinov <andrue.konst@gmail.com>
2023-12-03 14:48:36 +0300

    Update placeholder implementation: avoid using self-references for serialization

--- bosk/block/placeholder.py
+++ bosk/block/placeholder.py
@@ -7,12 +7,18 @@ class PlaceholderFunction:
     Adds self to the active builder.
     """
     def __init__(self, block):
-        self.__block = block
+        if block is self:
+            self.__block = None
+            self.__block_is_self = True
+        else:
+            self.__block = block
+            self.__block_is_self = False
 
     def __call__(self, *pfn_args, **pfn_kwargs) -> 'FunctionalBlockWrapper':
         builder = BoskState().active_builders.peek()
         assert builder is not None, \
             'No active builder found. ' \
             'Please, enter a builder scope by `with FunctionalPipelineBuilder() as b:`'
-        return builder.wrap(self.__block)(*pfn_args, **pfn_kwargs)
+        block = self if self.__block_is_self else self.__block
+        return builder.wrap(block)(*pfn_args, **pfn_kwargs)
 
